569cae0c7b038cbd312502f34518017a3ba34813
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-01-27 20:26:08 +0530

    Create LC 1 Two Sum.java
    First question solved!

---
+++ LC 1 Two Sum.java
@@ -0,0 +1,17 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i =0; i < nums.length; i++){
+                int num = nums[i];
+                int needed = target -num;
+
+                if (map.containsKey(needed)) {
+                    return new int[] { map.get(needed), i };
+            }
+                map.put(num, i);
+            }
+            return new int [] {};
+        }
+    }
