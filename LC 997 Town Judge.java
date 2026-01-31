b4f25f2ab37d0ec04a38e8a47a8e2f5c03d7e0a7
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-01-31 15:36:38 +0530

    Create LC 997 Town Judge.java

---
+++ LC 997 Town Judge.java
@@ -0,0 +1,19 @@
+// LC 997. Find the Town Judge
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] trustCount = new int[n + 1];
+
+        for (int[] t : trust) {
+            trustCount[t[0]]--; 
+            trustCount[t[1]]++; 
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (trustCount[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
