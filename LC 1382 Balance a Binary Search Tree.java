e92e89198118a91c843feb6dab8cca349ba2237e
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-02-09 21:20:19 +0530

    Create LC 1382 Balance a Binary Search Tree.java
    Solved POD 9th Feb, 2026

---
+++ LC 1382 Balance a Binary Search Tree.java
@@ -0,0 +1,43 @@
+// LC 1382 Balance a Binary Search Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return buildTree(0, list.size() - 1);
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+
+    private TreeNode buildTree(int left, int right) {
+        if (left > right) return null;
+
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+
+        root.left = buildTree(left, mid - 1);
+        root.right = buildTree(mid + 1, right);
+
+        return root;
+    }
+}
