f845887b8f9a54ecee45b75b3cbc34644cc30700
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-02-11 19:59:46 +0530

    Create LC 1287 Element Appearing More Than 25% In Sorted Array.java

---
+++ LC 1287 Element Appearing More Than 25% In Sorted Array.java
@@ -0,0 +1,16 @@
+// 1287 Element Appearing More Than 25% In Sorted Array
+
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+        int step = n / 4;
+        
+        for (int i = 0; i < n - step; i++) {
+            if (arr[i] == arr[i + step]) {
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+}
