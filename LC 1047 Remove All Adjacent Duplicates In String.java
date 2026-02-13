428d3e2d624605c937d2ee70e93d2083ebe2dfc0
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-02-13 22:50:56 +0530

    Create LC 1047 Remove All Adjacent Duplicates In String.java

---
+++ LC 1047 Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,22 @@
+// LC 1047 Remove All Adjacent Duplicates In String
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop(); 
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char ch : stack) {
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
