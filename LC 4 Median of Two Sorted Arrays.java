0b90382442e11814ceadc0f0b786e02f541e1e81
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-02-13 23:09:33 +0530

    Create LC 4 Median of Two Sorted Arrays.java

---
+++ LC 4 Median of Two Sorted Arrays.java
@@ -0,0 +1,23 @@
+// LC 4 Median of Two Sorted Arrays
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[] merged = new int[m + n];
+
+        System.arraycopy(nums1, 0, merged, 0, m);
+        System.arraycopy(nums2, 0, merged, m, n);
+
+        java.util.Arrays.sort(merged);
+
+        int len = merged.length;
+
+        if (len % 2 == 0) {
+            return (merged[len/2 - 1] + merged[len/2]) / 2.0;
+        } else {
+            return merged[len/2];
+        }
+    }
+}
