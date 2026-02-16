ba02dbe4af9a99ee96544394722c2bd4757987b2
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-02-16 11:07:27 +0530

    Create LC 46 Permutations.java

---
+++ LC 46 Permutations.java
@@ -0,0 +1,31 @@
+// LC 46. Permutations
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+    boolean[] used = new boolean[nums.length];
+    backtrack(nums, result, new ArrayList<>(), used);
+    return result;
+}
+
+private void backtrack(int[] nums, List<List<Integer>> result, 
+                       List<Integer> current, boolean[] used) {
+    
+    if (current.size() == nums.length) {
+        result.add(new ArrayList<>(current));
+        return;
+    }
+    
+    for (int i = 0; i < nums.length; i++) {
+        if (used[i]) continue;
+        
+        used[i] = true;
+        current.add(nums[i]);
+        
+        backtrack(nums, result, current, used);
+        
+        current.remove(current.size() - 1);
+        used[i] = false;
+        }
+    }
+}
