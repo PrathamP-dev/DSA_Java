707a2fefa24d941d5951f0a0a49baa1a73257141
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-08 17:07:52 +0530

    POD LC 1980 Find Unique Binary String.java
    This is the POD for 8th March, 2026

---
+++ LC 1980 Find Unique Binary String.java
@@ -0,0 +1,16 @@
+// LC 1980 Find Unique Binary String
+// This is the POD for 8th March, 2026
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < nums.length; i++) {
+            char c = nums[i].charAt(i);
+            if (c == '0') res.append('1');
+            else res.append('0');
+        }
+
+        return res.toString();
+    }
+}
