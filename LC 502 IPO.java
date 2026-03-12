3f113858e3c359f816b5f8f22ad581fd2cdcde74
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-12 21:05:04 +0530

    Create LC 502 IPO.java

---
+++ LC 502 IPO.java
@@ -0,0 +1,27 @@
+// LC 502 IPO
+
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        PriorityQueue<int[]> minCapital = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        PriorityQueue<Integer> maxProfit = new PriorityQueue<>((a,b)->b-a);
+
+        for(int i=0;i<n;i++){
+            minCapital.add(new int[]{capital[i], profits[i]});
+        }
+
+        for(int i=0;i<k;i++){
+
+            while(!minCapital.isEmpty() && minCapital.peek()[0] <= w){
+                maxProfit.add(minCapital.poll()[1]);
+            }
+
+            if(maxProfit.isEmpty()) break;
+
+            w += maxProfit.poll();
+        }
+
+        return w;
+    }
+}
