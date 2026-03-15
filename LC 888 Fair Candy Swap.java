a266652467c5accf849dbc057d8f15489545ba46
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-15 12:31:45 +0530

    Create LC 888 Fair Candy Swap.java

---
+++ LC 888 Fair Candy Swap.java
@@ -0,0 +1,24 @@
+// LC 888 Fair Candy Swap
+
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = 0, sumB = 0;
+        
+        for (int a : aliceSizes) sumA += a;
+        for (int b : bobSizes) sumB += b;
+        
+        int diff = (sumA - sumB) / 2;
+        
+        Set<Integer> setB = new HashSet<>();
+        for (int b : bobSizes) setB.add(b);
+        
+        for (int a : aliceSizes) {
+            int b = a - diff;
+            if (setB.contains(b)) {
+                return new int[]{a, b};
+            }
+        }
+        
+        return new int[]{};
+    }
+}
