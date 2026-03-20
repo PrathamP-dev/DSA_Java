e94b1a1d309dab0df03a4966bde7a9e506fd7022
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-20 20:24:38 +0530

    Create LC 76 Minimum Window Substring.java

---
+++ LC 76 Minimum Window Substring.java
@@ -0,0 +1,25 @@
+// LC 76 Minimum Window Substring
+
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) map[c]++;
+
+        int left = 0, count = t.length(), minLen = Integer.MAX_VALUE, start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (map[s.charAt(right)]-- > 0) count--;
+
+            while (count == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                if (map[s.charAt(left++)]++ == 0) count++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
