6f830dc5f0bab6cd0e33e5bf7273e85e954535d4
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-21 13:52:20 +0530

    POD LC 3643 Flip Square Submatrix Vertically.java
    This is the POD for 21th March, 2026

---
+++ LC 3643 Flip Square Submatrix Vertically.java
@@ -0,0 +1,16 @@
+// LC 3643 Flip Square Submatrix Vertically
+// This is the POD for 21th March, 2026
+
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        for (int i = x; i < x + k / 2; i++) {
+            int i2 = x + k - 1 - (i - x);
+            for (int j = y; j < y + k; j++) {
+                int t = grid[i][j];
+                grid[i][j] = grid[i2][j];
+                grid[i2][j] = t;
+            }
+        }
+        return grid;
+    }
+}
