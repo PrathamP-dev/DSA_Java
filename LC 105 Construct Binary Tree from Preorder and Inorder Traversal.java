c495351e276ecb39ad9f52d9bd64a145ff7604a8
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-03-30 20:25:21 +0530

    Create LC 105 Construct Binary Tree from Preorder and Inorder Traversal.java

---
+++ LC 105 Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,45 @@
+// LC 105 Construct Binary Tree from Preorder and Inorder Traversal
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int[] inorder, int left, int right) {
+        if (left > right) return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = findIndex(inorder, rootVal);
+
+        root.left = helper(preorder, inorder, left, index - 1);
+        root.right = helper(preorder, inorder, index + 1, right);
+
+        return root;
+    }
+
+    private int findIndex(int[] inorder, int val) {
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == val) return i;
+        }
+        return -1;
+    }
+}
