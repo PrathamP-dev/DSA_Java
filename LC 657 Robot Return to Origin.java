52b3a93d4494f34b8ff5f1291249dd42046056cc
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2026-04-05 20:30:27 +0530

    POD LC 657 Robot Return to Origin.java
    This is the POD for 05th April, 2026

---
+++ LC 657 Robot Return to Origin.java
@@ -0,0 +1,17 @@
+// LC 657 Robot Return to Origin
+// This is the POD for 05th April, 2026
+
+class Solution {
+    public boolean judgeCircle(String moves) {
+       int x = 0, y = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') y++;
+            else if (c == 'D') y--;
+            else if (c == 'L') x--;
+            else if (c == 'R') x++;
+        }
+
+        return x == 0 && y == 0; 
+    }
+}
